032937c51be506534392010b25725fe028ab9507
DeivysGrail <66143151+DeivysGrail@users.noreply.github.com>
2024-04-25 15:05:58 +0200

    Update Loose.jsx

--- src/components/Loose.jsx
+++ src/components/Loose.jsx
@@ -4,7 +4,7 @@ import("../style/clicker.scss")
 
 function Loose(props) {
     return (
-        <div className={"MAIN__LOOSE"}><h1 className={"MAIN__LOOSE--h1"}>PERDU</h1><h2>Ton score est de {props.score}</h2></div>);
+        <div className={"MAIN__LOOSE"}><h1 className={"MAIN__LOOSE--h1"}>PERDU</h1><h2>Ton score est de {props.score}</h2><button onClick={props.tryAgain}>RÉESSAYER</button></div>);
 }
 
 export default Loose;
