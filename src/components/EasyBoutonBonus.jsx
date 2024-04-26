7f88302dcc34dbbfafe285169ec81b7e2a3b238d
DeivysGrail <66143151+DeivysGrail@users.noreply.github.com>
2024-04-26 14:22:22 +0200

    Update EasyBoutonBonus.jsx
    Changement de fiche de style + correction placement

--- src/components/EasyBoutonBonus.jsx
+++ src/components/EasyBoutonBonus.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import "../style/BonusButton.scss"
-
+import "../style/clicker.scss"
 function EasyBoutonBonus(props) {
     function getRandomNumber(min, max) {
 
@@ -17,9 +16,10 @@ function EasyBoutonBonus(props) {
         return Math.round(getRandomNumber(1, 2))
 
     })
-    const [ready, setReady] = useState(false)
-    const [left, setLeft] = useState(Math.round((Math.random() * 95) ))
-    const [top, setTop] = useState(Math.round((Math.random() * 100) ))
+
+    const [ready, setReady] = useState(true)
+    const [left, setLeft] = useState(Math.round((Math.random() * 80) ))
+    const [top, setTop] = useState(Math.round((Math.random() * 80) ))
 
     function boxReady() {
         setReady(true)
@@ -37,19 +37,19 @@ function EasyBoutonBonus(props) {
     }, [triggerBox])
 
 
-    const EASY_BUTTON_1 = <div onClick={props.easy1} className={"EasyButton-1"} style={{left: `${left}vw`, top: `${top}vh`}}>
+    const EASY_BUTTON_1 = <div onClick={props.easy1} className={"MAIN__EASY_BUTTON--EasyButton-1 EasyButton-1"} style={{left: `${left}%`, top: `${top}%`}}>
         <button className="EasyButton-1__button"
                 onClick={() => setReady(false)}>+ 10pt
         </button>
     </div>
 
-    const EASY_BUTTON_2 = <div onClick={props.easy2} className={"EasyButton-2"} style={{left: `${left}vw`, top: `${top}vh`}}>
+    const EASY_BUTTON_2 = <div onClick={props.easy2} className={"MAIN__EASY_BUTTON--EasyButton-2  EasyButton-2"} style={{left: `${left}%`, top: `${top}%`}}>
         <button className="EasyButton-2__button"
                 onClick={() => setReady(false)}>Stop 3s
         </button>
     </div>
 
-    const EASY_BUTTON_3 = <div onClick={props.easy3} className={"EasyButton-3"} style={{left: `${left}vw`, top: `${top}vh`}}>
+    const EASY_BUTTON_3 = <div onClick={props.easy3} className={"MAIN__EASY_BUTTON--EasyButton-3  EasyButton-3"} style={{left: `${left}%`, top: `${top}%`}}>
         <button className="EasyButton-3__button"
                 onClick={() => setReady(false)}>⬆ 5px
         </button>
@@ -57,7 +57,7 @@ function EasyBoutonBonus(props) {
 
 
     return (
-        <div>
+        <div className={"MAIN__EASY_BUTTON"}>
             {(ready && triggerBox === 1) && EASY_BUTTON_1}
             {(ready && triggerBox === 2) && EASY_BUTTON_2}
             {(ready && triggerBox === 3) && EASY_BUTTON_3}
