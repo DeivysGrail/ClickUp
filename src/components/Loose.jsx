9c4bdfb1e15c3a37b02d255a106bcff0854bfd7e
DeivysGrail <66143151+DeivysGrail@users.noreply.github.com>
2024-04-30 10:14:45 +0200

    Update Loose.jsx

--- src/components/Loose.jsx
+++ src/components/Loose.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import close_button from "../assets/close_button.png";
 
 import("../style/clicker.scss")
 
 function Loose(props) {
     return (
-        <div className={"MAIN__LOOSE"}><h1 className={"MAIN__LOOSE--h1"}>PERDU</h1>
-        <h2>Ton score est de {props.score}</h2>
-        <p>Meilleur score : {props.highScore}</p>
+        <div className={"MAIN__LOOSE"}><h1 className={"MAIN__LOOSE--text"}>PERDU</h1>
+        <h2 className={"MAIN__LOOSE--score"}>Ton score est de {props.score}</h2>
+        <p className={"MAIN__LOOSE--highScore"}>Meilleur score : {props.highScore}</p>
         <button className={"MAIN__LOOSE--button"} onClick={props.tryAgain}>RÉESSAYER</button>
+        <button className={"MAIN__LOOSE--openButton"} onClick={props.triggerInfo}>Info</button>
+            <img src={close_button} style={props.actif ? {visibility:"visible"} : {visibility: "hidden"}} className={"MAIN__LOOSE--closeButton"} onClick={props.trigger}/>
+
         </div>
 
     );
